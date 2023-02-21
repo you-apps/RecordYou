1b478962567b0564046a93d12333ff3ab2354b7a
Bnyro <bnyro@tutanota.com>
2023-02-21 09:34:40 +0100

    Improve the nothing here design

--- app/src/main/java/com/bnyro/recorder/ui/components/PlayerView.kt
+++ app/src/main/java/com/bnyro/recorder/ui/components/PlayerView.kt
@@ -2,20 +2,25 @@ package com.bnyro.recorder.ui.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ImportContacts
+import androidx.compose.material.icons.filled.AudioFile
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.bnyro.recorder.R
 import com.bnyro.recorder.ui.models.PlayerModel
 
 @Composable
@@ -40,13 +45,18 @@ fun PlayerView(
     } else {
         Box(modifier = Modifier.fillMaxSize()) {
             Column(
-                modifier = Modifier.align(Alignment.Center)
+                modifier = Modifier
+                    .align(Alignment.Center),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Icon(
                     modifier = Modifier.size(120.dp),
-                    imageVector = Icons.Default.ImportContacts,
+                    imageVector = Icons.Default.AudioFile,
                     contentDescription = null
                 )
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(text = stringResource(R.string.nothing_here))
+                Spacer(modifier = Modifier.height(100.dp))
             }
         }
     }
