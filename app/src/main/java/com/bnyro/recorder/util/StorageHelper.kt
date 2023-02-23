f616a5b21c61404db68f5ab30c892d336a253d9b
Bnyro <bnyro@tutanota.com>
2023-02-23 16:15:00 +0100

    Improve file names (closes #12)

--- app/src/main/java/com/bnyro/recorder/util/StorageHelper.kt
+++ app/src/main/java/com/bnyro/recorder/util/StorageHelper.kt
@@ -1,18 +1,21 @@
 package com.bnyro.recorder.util
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.net.Uri
-import android.util.Log
 import androidx.documentfile.provider.DocumentFile
 import com.bnyro.recorder.App
+import java.text.SimpleDateFormat
 import java.util.Calendar
 
 object StorageHelper {
+    @SuppressLint("SimpleDateFormat")
+    private val dateTimeFormat = SimpleDateFormat("yyyy-MM-dd_HH-mm-ss")
+
     fun getOutputFile(context: Context, extension: String): DocumentFile {
-        val currentTime = Calendar.getInstance().time.toString().replace(" ", "_")
+        val currentTime = dateTimeFormat.format(Calendar.getInstance().time)
 
         val recordingFile = getOutputDir(context).createFile("audio/*", "$currentTime.$extension")
-        Log.d("TAG", "Created file: " + recordingFile?.name)
         return recordingFile!!
     }
 
