a1df231da1382d3319dd0f92f31837d24ecbb0a1
Bnyro <bnyro@tutanota.com>
2023-04-12 08:58:06 +0200

    Use a collapsable top app bar for the player screen

--- app/src/main/java/com/bnyro/recorder/ui/common/FullscreenDialog.kt
+++ app/src/main/java/com/bnyro/recorder/ui/common/FullscreenDialog.kt
@@ -6,11 +6,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
@@ -21,6 +25,7 @@ import com.bnyro.recorder.R
 fun FullscreenDialog(
     title: String = "",
     onDismissRequest: () -> Unit,
+    useLargeAppBar: Boolean = false,
     actions: @Composable RowScope.() -> Unit = {},
     content: @Composable () -> Unit
 ) {
@@ -30,22 +35,49 @@ fun FullscreenDialog(
             usePlatformDefaultWidth = false
         )
     ) {
+        val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(
+            rememberTopAppBarState()
+        )
+
         Scaffold(
+            modifier = if (useLargeAppBar) {
+                Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+            } else {
+                Modifier
+            },
             topBar = {
-                TopAppBar(
-                    title = {
-                        Text(title)
-                    },
-                    navigationIcon = {
-                        ClickableIcon(
-                            imageVector = Icons.Default.ArrowBack,
-                            contentDescription = stringResource(R.string.back)
-                        ) {
-                            onDismissRequest.invoke()
-                        }
-                    },
-                    actions = actions
-                )
+                if (useLargeAppBar) {
+                    LargeTopAppBar(
+                        title = {
+                            Text(title)
+                        },
+                        navigationIcon = {
+                            ClickableIcon(
+                                imageVector = Icons.Default.ArrowBack,
+                                contentDescription = stringResource(R.string.back)
+                            ) {
+                                onDismissRequest.invoke()
+                            }
+                        },
+                        actions = actions,
+                        scrollBehavior = scrollBehavior
+                    )
+                } else {
+                    TopAppBar(
+                        title = {
+                            Text(title)
+                        },
+                        navigationIcon = {
+                            ClickableIcon(
+                                imageVector = Icons.Default.ArrowBack,
+                                contentDescription = stringResource(R.string.back)
+                            ) {
+                                onDismissRequest.invoke()
+                            }
+                        },
+                        actions = actions
+                    )
+                }
             }
         ) { pV ->
             Box(
--- app/src/main/java/com/bnyro/recorder/ui/screens/PlayerScreen.kt
+++ app/src/main/java/com/bnyro/recorder/ui/screens/PlayerScreen.kt
@@ -1,21 +1,15 @@
 package com.bnyro.recorder.ui.screens
 
-import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.bnyro.recorder.R
@@ -28,20 +22,14 @@ fun PlayerScreen(
     showVideoModeInitially: Boolean,
     onDismissRequest: () -> Unit
 ) {
-    val orientation = LocalConfiguration.current.orientation
     var showDeleteAllDialog by remember {
         mutableStateOf(false)
     }
 
     FullscreenDialog(
-        title = if (
-            orientation == Configuration.ORIENTATION_LANDSCAPE
-        ) {
-            stringResource(R.string.recordings)
-        } else {
-            ""
-        },
+        title = stringResource(R.string.recordings),
         onDismissRequest = onDismissRequest,
+        useLargeAppBar = true,
         actions = {
             ClickableIcon(
                 imageVector = Icons.Default.Delete,
@@ -56,14 +44,6 @@ fun PlayerScreen(
                 horizontal = 20.dp
             )
         ) {
-            if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-                Spacer(modifier = Modifier.height(70.dp))
-                Text(
-                    text = stringResource(R.string.recordings),
-                    fontSize = MaterialTheme.typography.headlineMedium.fontSize
-                )
-                Spacer(modifier = Modifier.height(15.dp))
-            }
             PlayerView(showVideoModeInitially, showDeleteAllDialog) {
                 showDeleteAllDialog = false
             }
