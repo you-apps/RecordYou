718c6d0f63df8e1f3b4ed8544679e7cb3240b9ca
Suhas Dissanayake <suhasdissa@gmail.com>
2023-10-06 18:39:38 +0530

    fix: Trim button appearing behind the home button

--- app/src/main/java/com/bnyro/recorder/ui/screens/TrimmerScreen.kt
+++ app/src/main/java/com/bnyro/recorder/ui/screens/TrimmerScreen.kt
@@ -12,8 +12,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -196,6 +198,7 @@ fun TrimmerScreen(onDismissRequest: () -> Unit, inputFile: DocumentFile) {
                     Text(stringResource(R.string.start_trimming))
                 }
             }
+            Spacer(Modifier.height(64.dp))
         }
     }
     with(trimmerModel) {
